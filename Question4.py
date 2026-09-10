1699763e3de89f42b11f3ad7103eb71627286b04
gouldmaj <gouldmaj@mail.lcc.edu>
2026-09-10 20:40:59 -0300

    main things done

--- Question4.py
+++ Question4.py
@@ -4,3 +4,9 @@ item2_price = float(input("Enter the price of the second item: $"))
 item3_price = float(input("Enter the price of the third item: $"))
 item4_price = float(input("Enter the price of the fourth item: $"))
 item5_price = float(input("Enter the price of the fifth item: $"))
+
+# This code calculates the total cost of the five items with a 7% sales tax
+total_cost = (item1_price + item2_price + item3_price + item4_price + item5_price) * 1.07
+
+# This code displays the total cost to the user
+print('The total cost of your purchase is $',total_cost,'.')
