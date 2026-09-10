94d7900ab80489da500559e387be2e6272bb24dd
gouldmaj <gouldmaj@mail.lcc.edu>
2026-09-10 20:06:22 -0300

    Main changes

---
+++ Question1.py
@@ -0,0 +1,5 @@
+# This code asks for the user's height, then displays it in a predetermined sentence
+height = int(input('Are you a shorty or a lamppost? Write your height in meters :\)  '))
+
+# This code displays the user's height to them
+print(f'Seems you\'re just {height} m tall... what a bozo.')
