26b60eccba9f37066a13dde544fa54e8eaa00d75
gouldmaj <gouldmaj@mail.lcc.edu>
2026-09-10 20:37:33 -0300

    Update Question4.py

--- Question4.py
+++ Question4.py
@@ -1,2 +1,6 @@
 # This code collects the data needed to calculate the total cost of a five-item sale
 item1_price = float(input("Enter the price of the first item: $"))
+item2_price = float(input("Enter the price of the second item: $"))
+item3_price = float(input("Enter the price of the third item: $"))
+item4_price = float(input("Enter the price of the fourth item: $"))
+item5_price = float(input("Enter the price of the fifth item: $"))
