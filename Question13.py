49fc61205ff8213cab1ab5bdcc24749b2348ddfe
gouldmaj <gouldmaj@mail.lcc.edu>
2026-09-10 21:11:42 -0300

    Update Question13.py

--- Question13.py
+++ Question13.py
@@ -6,3 +6,9 @@ end_post_assembly = float(input("Enter the amount of distance used by the end-po
 
 # This code asks for the amount of distance, in feet between vines
 vine_distance = float(input("Enter the amount of distance between vines in feet: "))
+
+# This code calculates the number of vines that can be planted in the row using the values provided by the user
+vines_amount = (row_length - 2 * end_post_assembly) / vine_distance
+
+# This code rounds the number of vines to the nearest whole number so it can be displayed as a whole number
+vines_amount = round(vines_amount)
