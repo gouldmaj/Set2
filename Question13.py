409848f1e2fec9b6e5c4794aa050729bbd4aeaf7
gouldmaj <gouldmaj@mail.lcc.edu>
2026-09-10 21:07:59 -0300

    Create Question13.py

---
+++ Question13.py
@@ -0,0 +1,5 @@
+# This code asks for the length of a given row, in feet
+row_length = float(input("Enter the length of the row in feet: "))
+
+#This code asks for the amount of space, in feet used by the end-post assembly
+end_post_space = float(input("Enter the amount of space used by the end-post assembly in feet: "))
