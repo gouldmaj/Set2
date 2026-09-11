3771b7c863012c545ad5028dd75c0bbb2ce98295
gouldmaj <gouldmaj@mail.lcc.edu>
2026-09-10 21:09:38 -0300

    Update Question13.py

--- Question13.py
+++ Question13.py
@@ -1,5 +1,8 @@
 # This code asks for the length of a given row, in feet
 row_length = float(input("Enter the length of the row in feet: "))
 
-#This code asks for the amount of space, in feet used by the end-post assembly
-end_post_space = float(input("Enter the amount of space used by the end-post assembly in feet: "))
+# This code asks for the amount of distance, in feet used by the end-post assembly
+end_post_assembly = float(input("Enter the amount of distance used by the end-post assembly in feet: "))
+
+# This code asks for the amount of distance, in feet between vines
+vine_distance = float(input("Enter the amount of distance between vines in feet: "))
